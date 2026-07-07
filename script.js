9cf1682efb4e78121f3056f72d44235c071382c3
Badshah1014 <110537515+Badshah1014@users.noreply.github.com>
2026-07-07 15:46:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,64 @@
 const stars=document.getElementById("stars");
 
-for(let i=0;i<250;i++){
+for(let i=0;i<200;i++){
 
-let star=document.createElement("div");
+let s=document.createElement("div");
 
-star.className="star";
+s.style.position="absolute";
+s.style.width="2px";
+s.style.height="2px";
+s.style.background="white";
+s.style.borderRadius="50%";
 
-let size=Math.random()*3+1;
+s.style.left=Math.random()*100+"vw";
+s.style.top=Math.random()*100+"vh";
 
-star.style.width=size+"px";
-star.style.height=size+"px";
+s.animate([
+{opacity:.2},
+{opacity:1},
+{opacity:.2}
+],{
+duration:2000+Math.random()*3000,
+iterations:Infinity
+});
 
-star.style.left=Math.random()*100+"vw";
-
-star.style.top=Math.random()*100+"vh";
-
-star.style.animationDuration=
-(Math.random()*3+2)+"s";
-
-stars.appendChild(star);
+stars.appendChild(s);
 
 }
 
-const btn=document.getElementById("startBtn");
+const story=[
+"Hello... 🌸",
+"I've been searching for you.",
+"Today is someone's very special day.",
+"Will you help me deliver a birthday wish? ✨"
+];
 
-btn.onclick=()=>{
+let index=0;
 
-document.body.style.transition="1.5s";
+const text=document.getElementById("text");
 
-document.body.style.opacity="0";
+document.getElementById("startBtn").onclick=()=>{
 
-setTimeout(()=>{
+document.getElementById("landing").classList.add("hidden");
 
-alert("🚧 Scene 2 Coming Next!");
+document.getElementById("story").classList.remove("hidden");
 
-},1500);
+text.innerHTML=story[index];
+
+}
+
+document.getElementById("nextBtn").onclick=()=>{
+
+index++;
+
+if(index<story.length){
+
+text.innerHTML=story[index];
+
+}else{
+
+alert("⭐ Next: The Star Collection Mini-Game!");
+
+}
 
 }
